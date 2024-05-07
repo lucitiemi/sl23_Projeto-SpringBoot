d1363d81aa2e4d7d0842c6d44d3f055c666c3e4a
Luciana Tiemi <lt.shimoyama@gmail.com>
2024-05-07 17:44:29 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -0,0 +1,81 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	// atributos basicos
+	@Id														// indica que o atributo id será a chave primária dessa entidade
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		// id com autoincremento (gerada automaticamente)
+	private Long id;
+	private String name;
+	
+	// atributos por associacao
+	
+	
+	
+	// contrutores
+	public Category() {
+	}
+
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	
+	
+	// getters e setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	// hashCode e equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+
+}
