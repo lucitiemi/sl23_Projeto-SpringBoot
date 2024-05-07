dc7a0795afd76691795293b17d71295ade8ffbb4
Luciana Tiemi <lt.shimoyama@gmail.com>
2024-05-07 15:25:05 -0300

    Order, Instant, ISO 8601 - PARTE 1

---
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,94 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	// atributos basicos
+	@Id														// indica que o atributo id será a chave primária dessa entidade
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		// id com autoincremento (gerada automaticamente)
+	private Long id;
+	private Instant moment;
+	
+	//atributos por associacoes
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	
+	// construtores
+	public Order() {
+	}
+
+	public Order(Long id, Instant moment, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+	}
+
+	
+	// getters e setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+	
+
+	
+	// heshCode e equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+
+}
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	// atributos por associacao
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	// construtores
 	public User() {
@@ -78,6 +86,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
 	
 	
 	// hashCode e equals
@@ -97,6 +109,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 	 
