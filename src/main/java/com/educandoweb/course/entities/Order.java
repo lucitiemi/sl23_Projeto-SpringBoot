bc0d46860202f9402fc53dd85eeb4096be9a3676
Luciana Tiemi <lt.shimoyama@gmail.com>
2024-05-07 16:43:32 -0300

    Order, Instant, ISO 8601 - PARTE 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +25,8 @@ public class Order implements Serializable {
 	@Id														// indica que o atributo id será a chave primária dessa entidade
 	@GeneratedValue(strategy = GenerationType.IDENTITY)		// id com autoincremento (gerada automaticamente)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
 	//atributos por associacoes
