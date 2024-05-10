3b9a8acc44e426072af6a2c336db54b3cfbac8b9
Luciana Tiemi <lt.shimoyama@gmail.com>
2024-05-09 21:29:48 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -3,7 +3,6 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import com.educandoweb.course.enums.OrderStatus;
@@ -126,7 +125,10 @@ public class Order implements Serializable {
 	// heshCode e equals
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -138,10 +140,11 @@ public class Order implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Order other = (Order) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	
-
 }
